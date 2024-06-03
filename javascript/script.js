1385ac445dcf38da538d7e594c2d929d56fb4c7b
kauazipf <rm558957@fiap.com.br>
2024-06-03 11:28:16 -0300

    Co-authored-by: Victor Egidio Lira <Vitin46@users.noreply.github.com> Co-authored-by: Caepena <Caepena@users.noreply.github.com>
    Criação, codificação e estilização da pagina de criação de artigos

--- javascript/script.js
+++ javascript/script.js
@@ -6,3 +6,23 @@ function toggleMenu() {
         menu.style.display = "block";
     }
 }
+
+
+
+function publicar(){
+    document.getElementById("form-artigos").addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    var title = document.getElementById("title").value;
+    var content = document.getElementById("content").value;
+    
+    var articleElement = document.createElement("div");
+    articleElement.classList.add("article");
+    articleElement.innerHTML = "<h2>" + title + "</h2><p>" + content + "</p>";
+    
+    document.getElementById("articles").appendChild(articleElement);
+    
+    document.getElementById("title").value = "";
+    document.getElementById("content").value = "";
+});
+}
